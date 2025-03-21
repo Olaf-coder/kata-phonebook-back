4e0df3fee81b09585deb03f92e410edb43fc4c82
Antoine MARTIN <antoinemartin@MacBookPro-M1-de-Antoine-Martin.local>
2025-03-21 15:25:02 +0100

    first commit

---
+++ src/main/java/com/kata/kataphonebookback/KataPhonebookBackApplication.java
@@ -0,0 +1,13 @@
+package com.kata.kataphonebookback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataPhonebookBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KataPhonebookBackApplication.class, args);
+    }
+
+}
