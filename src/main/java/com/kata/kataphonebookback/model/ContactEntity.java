21fad869e9152692e4134c6cbf88fe60a028b3ee
Antoine Martin <martin_a@etna-alternance.net>
2025-03-22 19:53:30 +0100

    add configuration for database access (H2) and model for contact

---
+++ src/main/java/com/kata/kataphonebookback/model/ContactEntity.java
@@ -0,0 +1,64 @@
+package com.kata.kataphonebookback.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="contact")
+public class ContactEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id", nullable = false, unique = true)
+    private Long id;
+
+    @Column(name="family_name", nullable = false)
+    private String familyName;
+
+    @Column(name="first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFamilyName() {
+        return familyName;
+    }
+
+    public void setFamilyName(String familyName) {
+        this.familyName = familyName;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
