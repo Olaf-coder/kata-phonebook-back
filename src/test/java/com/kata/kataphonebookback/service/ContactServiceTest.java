25f6ec36e33a8b0fa8ef262e7fa0083e1fe101f7
Antoine Martin <martin_a@etna-alternance.net>
2025-03-23 18:48:13 +0100

    Completion services methods without exceptions

--- src/test/java/com/kata/kataphonebookback/service/ContactServiceTest.java
+++ src/test/java/com/kata/kataphonebookback/service/ContactServiceTest.java
@@ -2,12 +2,15 @@ package com.kata.kataphonebookback.service;
 
 import com.kata.kataphonebookback.domain.model.ContactEntity;
 import com.kata.kataphonebookback.domain.repository.ContactRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
@@ -24,6 +27,119 @@ class ContactServiceTest {
     @BeforeEach
     void setUp() {
         contactService = new ContactServiceImpl(contactRepository);
+    }
+
+
+    @Test
+    void should_call_save_once_with_correct_values_and_return_saved_contact_when_addNewContact_is_called_with_full_contact_and_no_id() {
+        //GIVEN
+        ContactEntity inputContactEntity = createExistingContactEntity();
+        inputContactEntity.setId(null);
+
+        ContactEntity savedContactEntity = createExistingContactEntity();
+        Contact expectedContact = createExpectedContact();
+
+        Mockito.when(contactRepository.save(inputContactEntity)).thenReturn(savedContactEntity);
+
+
+        //WHEN
+        Contact actualContact = contactService.addNewContact(inputContactEntity);
+
+
+        //THEN
+        verify(contactRepository, times(1)).save(inputContactEntity);
+        assertThat(actualContact).isEqualTo(expectedContact);
+
+    }
+
+    @Test
+    void should_call_save_once_with_correct_values_and_return_saved_contact_when_addNewContact_is_called_with_only_mandatory_data_and_no_id() {
+        //GIVEN
+        ContactEntity inputContactEntity = createContactEntity(null, "John", "Smith", null, null);
+        ContactEntity savedContactEntity = createContactEntity(1L, "John", "Smith", null, null);
+        Contact expectedContact = createContact(1L, "John", "Smith", null, null);
+        Mockito.when(contactRepository.save(inputContactEntity)).thenReturn(savedContactEntity);
+
+
+        //WHEN
+        Contact actualContact = contactService.addNewContact(inputContactEntity);
+
+
+        //THEN
+        verify(contactRepository, times(1)).save(inputContactEntity);
+        assertThat(actualContact).isEqualTo(expectedContact);
+
+    }
+    //todo methodes en erreur
+
+    @Test
+    void should_call_deleteById_once_when_deleteContact_is_called_with_id() {
+        //GIVEN
+        Long id = 1L;
+        doNothing().when(contactRepository).deleteById(id);
+
+
+        //WHEN
+        contactService.deleteContact(id);
+
+
+        //THEN
+        verify(contactRepository, times(1)).deleteById(id);
+        Assertions.assertThatNoException().isThrownBy(()->contactService.deleteContact(id));
+    }
+
+    @Test
+    void should_call_getReferenceById_once_with_correct_values_and_return_existing_contact_when_getContactById_is_called_with_correct_id() {
+        //GIVEN
+        Long id = 1L;
+
+        ContactEntity existingContactEntity = createExistingContactEntity();
+        Contact expectedContact = createExpectedContact();
+        when(contactRepository.getReferenceById(id)).thenReturn(existingContactEntity);
+
+
+        //WHEN
+        Contact actualContact = contactService.getContactById(id);
+
+
+        //THEN
+        verify(contactRepository, times(1)).getReferenceById(id);
+        assertThat(actualContact).isEqualTo(expectedContact);
+    }
+    //todo invalid parameter => exception
+
+    @Test
+    void getAllContacts() {
+        //GIVEN
+        List<ContactEntity> contactEntities = createExistingContactEntities();
+        List<Contact> expectedContacts = createExpectedContacts();
+        Mockito.when(contactRepository.findAll()).thenReturn(contactEntities);
+
+        //WHEN
+        List<Contact> actualContacts = contactService.getAllContacts();
+
+        //THEN
+        verify(contactRepository, times(1)).findAll();
+        assertThat(actualContacts).isEqualTo(expectedContacts);
+    }
+    //todo invalid parameter => exception
+
+    private List<ContactEntity> createExistingContactEntities()
+    {
+        return List.of(createContactEntity(1L, "John", "Smith", null, null),
+                createContactEntity(2L, "William", "Saurin", "saurinwilliam@mail.com", "060102030405"),
+                createContactEntity(3L, "Sophie", "Saurin", "saurinsophie@mail.com", null),
+                createContactEntity(4L, "Michel", "Palaref", null, null)
+        );
+    }
+
+    private List<Contact> createExpectedContacts()
+    {
+        return List.of(createContact(1L, "John", "Smith", null, null),
+                createContact(2L, "William", "Saurin", "saurinwilliam@mail.com", "060102030405"),
+                createContact(3L, "Sophie", "Saurin", "saurinsophie@mail.com", null),
+                createContact(4L, "Michel", "Palaref", null, null)
+        );
     }
 
     private ContactEntity createExistingContactEntity() {
@@ -50,82 +166,5 @@ class ContactServiceTest {
 
     private Contact convertEntityToContact(ContactEntity contactEntity) {
         return new Contact(contactEntity.getId(), contactEntity.getFirstName(), contactEntity.getFamilyName(), contactEntity.getPhoneNumber(), contactEntity.getEmail());
-    }
-
-    @Test
-    void should_call_save_once_with_correct_values_when_addNewContact_is_called_with_full_contact_and_no_id() {
-        //GIVEN
-        ContactEntity inputContactEntity = createExistingContactEntity();
-        inputContactEntity.setId(null);
-
-        ContactEntity savedContactEntity = createExistingContactEntity();
-        Contact expectedContact = createExpectedContact();
-
-        Mockito.when(contactRepository.save(inputContactEntity)).thenReturn(savedContactEntity);
-
-
-        //WHEN
-        Contact actualContact = contactService.addNewContact(inputContactEntity);
-
-
-        //THEN
-        verify(contactRepository, times(1)).save(inputContactEntity);
-        assertThat(actualContact).isEqualTo(expectedContact);
-
-    }
-
-    @Test
-    void should_call_save_once_with_correct_values_when_addNewContact_is_called_with_only_mandatory_data_and_no_id() {
-        //GIVEN
-        ContactEntity inputContactEntity = createContactEntity(null, "John", "Smith", null, null);
-        ContactEntity savedContactEntity = createContactEntity(1L, "John", "Smith", null, null);
-        Contact expectedContact = createContact(1L, "John", "Smith", null, null);
-        Mockito.when(contactRepository.save(inputContactEntity)).thenReturn(savedContactEntity);
-
-
-        //WHEN
-        Contact actualContact = contactService.addNewContact(inputContactEntity);
-
-
-        //THEN
-        verify(contactRepository, times(1)).save(inputContactEntity);
-        assertThat(actualContact).isEqualTo(expectedContact);
-
-    }
-
-    @Test
-    void deleteContact() {
-        //GIVEN
-
-
-        //WHEN
-
-
-        //THEN
-
-    }
-
-    @Test
-    void getAllContacts() {
-        //GIVEN
-
-
-        //WHEN
-
-
-        //THEN
-
-    }
-
-    @Test
-    void getContactById() {
-        //GIVEN
-
-
-        //WHEN
-
-
-        //THEN
-
     }
 }
